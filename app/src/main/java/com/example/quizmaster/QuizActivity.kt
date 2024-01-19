4112e84f8d6628a8132765a76b5349139d6e8d40
botfigther02 <abuhurayraniloy02@gmail.com>
2024-01-20 01:30:19 +0600

    Timer

--- app/src/main/java/com/example/quizmaster/QuizActivity.kt
+++ app/src/main/java/com/example/quizmaster/QuizActivity.kt
@@ -1,6 +1,7 @@
 package com.example.quizmaster
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,6 +14,7 @@ class QuizActivity : AppCompatActivity() {
 
     companion object{
         var questionModelList : List<QuestionModel> = listOf()
+        var time : String = ""
     }
 
     lateinit var binding: ActivityQuizBinding
@@ -23,6 +25,24 @@ class QuizActivity : AppCompatActivity() {
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
         loadQuestions()
+        startTimer()
+    }
+
+    private fun startTimer(){
+        val totalTimeInMillis = time.toInt() * 60 * 1000L
+        object : CountDownTimer(totalTimeInMillis, 1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                val seconds = millisUntilFinished/1000
+                val minutes = seconds/60
+                val remainingSeconds = seconds%60
+                binding.timerIndicatorTextview.text = String.format("%02d:%02d", minutes, remainingSeconds)
+            }
+
+            override fun onFinish() {
+                //Finished the quiz
+            }
+
+        }.start()
     }
     private fun loadQuestions(){
         binding.apply {
@@ -33,7 +53,7 @@ class QuizActivity : AppCompatActivity() {
             btn0.text = questionModelList[currentQuestionIndex].options[0]
             btn1.text = questionModelList[currentQuestionIndex].options[1]
             btn2.text = questionModelList[currentQuestionIndex].options[2]
-            btn3.text = questionModelList[currentQuestionIndex].options[2]
+            btn3.text = questionModelList[currentQuestionIndex].options[3]
         }
     }
 }
