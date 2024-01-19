a7fd007f63871833c09df2604c5fd4615607c43e
botfigther02 <abuhurayraniloy02@gmail.com>
2024-01-19 23:16:53 +0600

    Testing UI with dummy data

--- app/src/main/java/com/example/quizmaster/MainActivity.kt
+++ app/src/main/java/com/example/quizmaster/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.quizmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quizmaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding;
-    
+    lateinit var quiModelList: MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        quiModelList = mutableListOf()
+        getDataFromFirebase()
+    }
+
+    private fun setupRecyclerView(){
+        adapter = QuizListAdapter(quiModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase(){
+        quiModelList.add(QuizModel("1","Programming","All the basic progarmming","10"))
+        quiModelList.add(QuizModel("2","Computer","All the basic computer question","20"))
+        quiModelList.add(QuizModel("3","Programming","All the ger question","15"))
+        setupRecyclerView()
     }
 }
---
+++ app/src/main/java/com/example/quizmaster/QuizListAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.quizmaster
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quizmaster.databinding.QuizItemRecyclerRowBinding
+
+class QuizListAdapter(private val quizModelList : List<QuizModel>) :
+    RecyclerView.Adapter<QuizListAdapter.MyViewHolder>() {
+    class MyViewHolder(private val binding: QuizItemRecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(model : QuizModel){
+            binding.apply {
+                quizTitleText.text = model.title
+                quizSubtitleText.text = model.subtitle
+                quizTimeText.text = model.time
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = QuizItemRecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return quizModelList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(quizModelList[position])
+    }
+}
